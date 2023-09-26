4c364a43343446b0a501e43dcffa21595d4dc838
George <georgeneves.dev@gmail.com>
2023-09-26 16:11:43 -0300

    implementando conceitos de lista

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,13 +1,38 @@
 package application;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Program
 {
-    public static void main( String[] args )
+    public static void main(String... args )
     {
-        System.out.println( "Hello World!" );
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Bob");
+        list.add("Anna");
+        list.add(2, "Marco");
+
+        for (String x : list){ System.out.println(x);}
+
+        System.out.println("---------------------");
+        list.removeIf(x -> x.charAt(0) == 'M');
+
+        for (String x : list){ System.out.println(x);}
+
+        System.out.println("---------------------");
+        System.out.println("Index of Bob: " + list.indexOf("Bob"));
+        System.out.println("Index of Marco: " + list.indexOf("Marco"));
+
+        System.out.println("---------------------");
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String x : result){ System.out.println(x);}
+
+        System.out.println("---------------------");
+        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        System.out.println(name);
     }
 }
